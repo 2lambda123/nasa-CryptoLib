68f53ce1076adbed0275f3944864dd482d1e4d81
Restyled.io <commits@restyled.io>
2024-09-04 20:10:31 +0000

    Restyled by shellcheck

--- support/scripts/docker_debug.sh
+++ support/scripts/docker_debug.sh
@@ -6,8 +6,8 @@
 #
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/env.sh
+source "$SCRIPT_DIR"/env.sh
 
 echo "Start docker container to debug in..."
-$DFLAGS -v $BASE_DIR:$BASE_DIR -w $BASE_DIR $DBOX bash
+$DFLAGS -v "$BASE_DIR":"$BASE_DIR" -w "$BASE_DIR" "$DBOX" bash
 echo ""
--- support/scripts/internal_docker_build.sh
+++ support/scripts/internal_docker_build.sh
@@ -6,16 +6,16 @@
 #
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/env.sh
+source "$SCRIPT_DIR"/env.sh
 
 # Prepare build directory
-mkdir $BASE_DIR/build > /dev/null 2>&1
-rm -r $BASE_DIR/build/internal/* > /dev/null 2>&1
-mkdir $BASE_DIR/build/internal > /dev/null 2>&1
+mkdir "$BASE_DIR"/build > /dev/null 2>&1
+rm -r "$BASE_DIR"/build/internal/* > /dev/null 2>&1
+mkdir "$BASE_DIR"/build/internal > /dev/null 2>&1
 
 #$DFLAGS -v $BASE_DIR:$BASE_DIR -w $BASE_DIR/build/internal $DBOX /bin/bash
 
 echo "Internal build and test..."
-$DFLAGS -v $BASE_DIR:$BASE_DIR -w $BASE_DIR/build/internal $DBOX bash -c \
+$DFLAGS -v "$BASE_DIR":"$BASE_DIR" -w "$BASE_DIR"/build/internal "$DBOX" bash -c \
     "../../support/scripts/build_support.sh"
 echo ""
